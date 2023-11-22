9f5c5be7eee6db3c096a430a985435d8e23e2916
edu-silva-dev <147456509+edu-silva-dev@users.noreply.github.com>
2023-11-22 15:09:37 -0300

    exercicio numero par js

---
+++ exercicios-feaDev/Aulas-Proz-JS/exemplos-js.js
@@ -0,0 +1,11 @@
+let numeroDasorte = [34 , 14 , 26 , 594 , 87]
+for(let i =0; i< numeroDasorte.length;i++){
+    if (numeroDasorte[i] % 2 ==0){
+        console.log(numeroDasorte[i],'é par')
+    }
+else if (numeroDasorte[i] != 0){
+    console.log(numeroDasorte[i],'não é par')
+}
+}
+
+
