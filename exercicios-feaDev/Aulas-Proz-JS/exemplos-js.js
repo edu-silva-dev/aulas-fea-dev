8222b641fd37f645e27694168b8e069500d5f304
edu-silva-dev <147456509+edu-silva-dev@users.noreply.github.com>
2023-11-23 11:47:06 -0300

    Adicionamos os exercicio

--- exercicios-feaDev/Aulas-Proz-JS/exemplos-js.js
+++ exercicios-feaDev/Aulas-Proz-JS/exemplos-js.js
@@ -1,11 +1,12 @@
-let numeroDasorte = [34 , 14 , 26 , 594 , 87]
+let numeroDasorte = [34 , 14 , 26 , 594 , 47]
 for(let i =0; i< numeroDasorte.length;i++){
-    if (numeroDasorte[i] % 2 ==0){
-        console.log(numeroDasorte[i],'é par')
+    if (numeroDasorte[i] % 2 ==0 && numeroDasorte[i] <= 50){
+        console.log('Numeros menores que 50 que são pares =', numeroDasorte[i])
     }
-else if (numeroDasorte[i] != 0){
-    console.log(numeroDasorte[i],'não é par')
+else if (numeroDasorte[i] != 0 && numeroDasorte[i] <= 50){
+    console.log('Numeros menores que 50 que não são pares =', numeroDasorte[i])
 }
 }
+
 
 
